ec8d64b1d788666bbbe828f089482967af12fb39
Jendrik Johannes <jendrik@gradle.com>
2021-02-26 16:19:00 +0100

    Use 'gradle-7.0-milestone-2'

--- agp-patch-plugin/src/main/kotlin/agp-patch.gradle.kts
+++ agp-patch-plugin/src/main/kotlin/agp-patch.gradle.kts
@@ -1,8 +1,5 @@
 // AGP7 should do this:
 configurations.whenObjectAdded {
-    if (name in listOf(JavaPlugin.COMPILE_CLASSPATH_CONFIGURATION_NAME, JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME)) {
-        attributes.attribute(TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE, objects.named(TargetJvmEnvironment.STANDARD_JVM))
-    }
     if (name in listOf("releaseRuntimeClasspath", "debugRuntimeClasspath", "releaseCompileClasspath", "debugCompileClasspath")) {
         attributes.attribute(TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE, objects.named(TargetJvmEnvironment.ANDROID)) // by Android plugin
     }
